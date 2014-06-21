f9ba4d2f399d16bc852bc3d4b3a76949c41dd924
Ramil Farkhshatov <ramil@gmx.co.uk>
2014-06-21 19:38:02 +0400

    Updated scheme klvm interpreter sample.

--- test/stage-2.scm
+++ test/stage-2.scm
@@ -79,6 +79,18 @@
            (newline port))
          x))
 
+(define (mkstr . args)
+  (with-output-to-string '()
+    (lambda () (for-each display args))))
+
+(define (strjoin lst sep)
+  (let loop ((lst lst)
+             (ret "")
+             (s ""))
+    (if (pair? lst)
+        (loop (cdr lst) (mkstr ret s (car lst)) sep)
+        ret)))
+
 (define (klvm-add-func func vm)
   (let ((prev (table-ref (klvm-fns vm) (klvm-closure-name func) #f)))
     (cond ((klvm-closure? prev)
@@ -317,12 +329,11 @@
                          (klvm-closure-code func)
                          (reverse args)))))
 
-(define (put-closure-args expr closure vm)
-  (let ((n (cadr expr))
-        (nargs (length (klvm-closure-vars closure))))
-    (klvm-ensure-regs-size! vm (+ nargs n))
+(define (put-closure-args offset closure vm)
+  (let ((nargs (length (klvm-closure-vars closure))))
+    (klvm-ensure-regs-size! vm (+ nargs offset))
     (let loop ((args (klvm-closure-vars closure))
-               (i (+ (klvm-sp vm) n)))
+               (i (+ (klvm-sp vm) offset)))
       (cond ((pair? args)
              (vector-set! (klvm-regs vm) i (car args))
              (loop (cdr args) (+ i 1)))
@@ -386,7 +397,8 @@
                ((klvm.push-error-handler)
                 (push-error-handler (eval-2 (cadr expr)) vm))
                ((klvm.pop-error-handler) (pop-error-handler vm))
-               ((klvm.put-closure-args) (put-closure-args expr closure vm))
+               ((klvm.put-closure-args)
+                (put-closure-args (cadr expr) closure vm))
                ((klvm.ret->) (set-klvm-ret! vm (eval-2 (cadr expr))))
                ((klvm.nregs->)
                 (klvm-ensure-regs-size! vm (apply + (map eval-2
@@ -487,56 +499,57 @@
   (reset-klvm! *vm*)
   (read-klvm-from-file "code.klvm2" *vm*))
 
+(define t1.defs
+  '((klvm-test.list-len ()) => 0
+    (klvm-test.list-len (1 2 3 4 a)) => 5
+    (klvm-test.test-call 9) => 23
+    (klvm-test.reversex ()) => ()
+    (klvm-test.reversex (1 2 3 4 5)) => (5 4 3 2 1)
+    (+ 7 8) => 15
+    (- 7 8) => -1
+    (klvm-test.test-partial) => 9
+    (klvm-test.test-closure) => 14
+    (klvm-test.test-closure-2) => (1 2 3 4 5)
+    (klvm-test.test-closure-3) => (1 2 3 4 5 klvm-test.a klvm-test.b)
+    (klvm-test.test-freeze) => 25
+    (klvm-test.test-closure-4) => 17
+    (klvm-test.test-closure-5) => 0
+    (klvm-test.test-map ()) => ()
+    (klvm-test.test-map (-1 2 -3 4 -5 6)) => (#f #t #f #t #f #t)
+    (klvm-test.incr-list (-1 2 -3 4 -5 6)) => (0 3 -2 5 -4 7)
+    (klvm-test.incr-list-aux (-1 2 -3 4 -5 6)) => (0 3 -2 5 -4 7)
+    (klvm-test.test-guard) => ((klvm-test.num 1)
+                               (klvm-test.sym klvm-test.two)
+                               (str "three")
+                               (str "four")
+                               (klvm-test.num 5)
+                               (klvm-test.sym klvm-test.six))
+    (klvm-test.test-do) => #(3 3 5 15)
+    (klvm-test.test-and #t #t) => #t
+    (klvm-test.test-and #t #f) => #f
+    (klvm-test.test-and #f #t) => #f
+    (klvm-test.test-and #f #f) => #f
+    (klvm-test.test-and-2 #t #t) => #t
+    (klvm-test.test-and-2 #t #f) => #f
+    (klvm-test.test-and-2 #f #t) => #f
+    (klvm-test.test-and-2 #f #f) => #f
+    (klvm-test.test-or #t #t) => #t
+    (klvm-test.test-or #f #t) => #t
+    (klvm-test.test-or #t #f) => #t
+    (klvm-test.test-or #f #f) => #f
+    (klvm-test.test-or-2 #t #t) => #t
+    (klvm-test.test-or-2 #f #t) => #t
+    (klvm-test.test-or-2 #t #f) => #t
+    (klvm-test.test-or-2 #f #f) => #f
+    (klvm-test.appendx (a b c) (2 3 4)) => (a b c 2 3 4)
+    (klvm-test.test-let-1 #t) => #(4 3 5 15 klvm-test.one)
+    (klvm-test.test-let-1 #f) => #(4 0 1 2 klvm-test.one)
+    (klvm-test.test-freeze-2) => 80
+    (klvm-test.test-trap-error) => 0
+    (klvm-test.test-trap-error-2) => 0
+    (klvm-test.test-trap-error-3) => 0
+    (klvm-test.test-trap-error-4) => 0))
+
 (define (t1)
   (t1.setup)
-  (test '(
-          (klvm-test.list-len ()) => 0
-          (klvm-test.list-len (1 2 3 4 a)) => 5
-          (klvm-test.test-call 9) => 23
-          (klvm-test.reversex ()) => ()
-          (klvm-test.reversex (1 2 3 4 5)) => (5 4 3 2 1)
-          (+ 7 8) => 15
-          (- 7 8) => -1
-          (klvm-test.test-partial) => 9
-          (klvm-test.test-closure) => 14
-          (klvm-test.test-closure-2) => (1 2 3 4 5)
-          (klvm-test.test-closure-3) => (1 2 3 4 5 klvm-test.a klvm-test.b)
-          (klvm-test.test-freeze) => 25
-          (klvm-test.test-closure-4) => 17
-          (klvm-test.test-closure-5) => 0
-          (klvm-test.test-map ()) => ()
-          (klvm-test.test-map (-1 2 -3 4 -5 6)) => (#f #t #f #t #f #t)
-          (klvm-test.incr-list (-1 2 -3 4 -5 6)) => (0 3 -2 5 -4 7)
-          (klvm-test.incr-list-aux (-1 2 -3 4 -5 6)) => (0 3 -2 5 -4 7)
-          (klvm-test.test-guard) => ((klvm-test.num 1)
-                                     (klvm-test.sym klvm-test.two)
-                                     (str "three")
-                                     (str "four")
-                                     (klvm-test.num 5)
-                                     (klvm-test.sym klvm-test.six))
-          (klvm-test.test-do) => #(3 3 5 15)
-          (klvm-test.test-and #t #t) => #t
-          (klvm-test.test-and #t #f) => #f
-          (klvm-test.test-and #f #t) => #f
-          (klvm-test.test-and #f #f) => #f
-          (klvm-test.test-and-2 #t #t) => #t
-          (klvm-test.test-and-2 #t #f) => #f
-          (klvm-test.test-and-2 #f #t) => #f
-          (klvm-test.test-and-2 #f #f) => #f
-          (klvm-test.test-or #t #t) => #t
-          (klvm-test.test-or #f #t) => #t
-          (klvm-test.test-or #t #f) => #t
-          (klvm-test.test-or #f #f) => #f
-          (klvm-test.test-or-2 #t #t) => #t
-          (klvm-test.test-or-2 #f #t) => #t
-          (klvm-test.test-or-2 #t #f) => #t
-          (klvm-test.test-or-2 #f #f) => #f
-          (klvm-test.appendx (a b c) (2 3 4)) => (a b c 2 3 4)
-          (klvm-test.test-let-1 #t) => #(4 3 5 15 klvm-test.one)
-          (klvm-test.test-let-1 #f) => #(4 0 1 2 klvm-test.one)
-          (klvm-test.test-freeze-2) => 80
-          (klvm-test.test-trap-error) => 0
-          (klvm-test.test-trap-error-2) => 0
-          (klvm-test.test-trap-error-3) => 0
-          (klvm-test.test-trap-error-4) => 0
-        )))
+  (test t1.defs))
